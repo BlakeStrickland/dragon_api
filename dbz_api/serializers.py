9ae310579283ec980013a14a52bf6741a6ac648e
Blake Strickland <Stricklandb02@gmail.com>
2016-05-14 18:02:23 -0400

    serializers and models

---
+++ dbz_api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Ability
+
+
+class CharacterStatisticsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CharacterStatistics
+        fields = ('name', 'birth_name', 'home_planet', 'power_level')
+
+class AbilitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ability
+        fields = ('character', 'name', 'damage', 'ability_range', 'minimum_power_to_use')
