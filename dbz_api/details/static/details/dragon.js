36d581e19eaaaa0c7ac44ef5ea65393e5b2c9bcc
Blake Strickland <Stricklandb02@gmail.com>
2016-05-17 15:15:28 -0400

    Give each div a unique id

--- dbz_api/details/static/details/dragon.js
+++ dbz_api/details/static/details/dragon.js
@@ -1,47 +1,19 @@
 var $characters = $('#character');
 var $power_level = $('#power_level');
 var $card = $(".card")
+var count = 1
+
 
 $.get('http://127.0.0.1:8000/api/character_statistics/', function(characters){
   characters.forEach(function(character) {
-    var $p = $('<li>');
-    var $p2 = $('<p>');
 
     $name = character.name
-    // $p.text(character.name);
-    // $p.appendTo($characters);
     $power_level = character.power_level
-    // $p2.text(character.power_level);
-    // $p2.appendTo($characters);
-    $(".card").append('<div class="stats"> '+ $name + "<br />" + $power_level + '</div>')
 
+    $(".card").append('<div class="stats" id='+count+'> '+ $name + "<br />" + $power_level + '</div>')
+    count++
   })
 })
 
 
 $( "li" ).first().hide();
-
-// my_list = $.get('http://127.0.0.1:8000/api/character_statistics/', function(response) {
-//   console.log(response.data)
-// })
-// console.log(my_list)
-
-
-
-// $.get('http://127.0.0.1:8000/api/character_statistics/'), function(list_characters){
-//   list_characters.forEach(function(list_characters) {
-//
-//   })
-// };
-
-
-// for (i = 0; i < my_list.length; i++) {
-//     console.log(my_list);
-// }
-
-
-// var x = myFunction(4, 3);        // Function is called, return value will end up in x
-//
-// function myFunction(a, b) {
-//     return a * b;                // Function returns the product of a and b
-// }
